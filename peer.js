4a496951b7ee2f11c0dcbc1125875d302a5f3477
kode-git <mariosessa64@gmail.com>
2022-01-22 06:35:51 -0800

    Setting webRTC connections offers and answers

--- peer.js
+++ peer.js
@@ -18,6 +18,7 @@ var isInitiator; // if the user is the creator and initial painter of the game
 var TURNReady; // if the TURN communication setup is done
 var offerChannel; // offerer channel
 var answerChannel; // answer channel
+var stateConnection; // state of the connection
 var roomId; // Peer will connect to this roomId
 // Default openess connection to set default port and IP (not signalling) for P2P connection
 var socket = io.connect() // socket opening
@@ -28,6 +29,15 @@ createRoom.addEventListener("click", makeLobby); // create Lobby as the initiali
 function settingId(){
   return '_' + Math.random().toString(36).substr(2, 9);
 }
+
+// Socket listeners
+
+// Event to trigger the room creation and setting the client as the initiator
+socket.on("init", function (room) {
+  console.log("Created room " + room);
+  isInitiator = true;
+});
+
 
 // procedure to estabilish a connection room to make a lobby
 function makeLobby(){
@@ -43,6 +53,20 @@ function makeLobby(){
     }
    // Setting random Id for the room. People must use it to join with friends
    roomId = settingId() 
+   if(!stateConnection){
+     // peer is not connected yet
+     // do signaling
+     socket.emit("createOrJoin", roomId, username)
+     sendMessage("AddingUser", roomId)
+     if(isInitiator){
+       initConnection();
+     }
+   }
+  }
+
+
+// Making a RTC Peer Connection with Offer and Answer channel setting
+function createPeerConnection(){
    // Opening RTC Connection
    try{
       peerConnection = new RTCPeerConnection(configuration)
@@ -102,7 +126,13 @@ function makeLobby(){
    }
 }
 
-// ICE candidates handler
+// Sending a message to a room
+function sendMessage(message, room) {
+  console.log("Client sending message: ", message, room);
+  socket.emit("message", message, room);
+}
+
+// ICE candidates handlers
 function handleIceCandidate(event) {
   console.log("ICE candidate event is: ", event);
   if (event.candidate) {
@@ -113,21 +143,76 @@ function handleIceCandidate(event) {
         id: event.candidate.sdpMid,
         candidate: event.candidate.candidate,
       },
-      room
+      roomId
     );
   } else {
     console.log("End of candidates.");
   }
 }
 
-// Socket listeners
+// Error handler on Offer and Answer Channel
+function handleOfferError(error){
+  console.log('Error on offer channel: ' + error)
+}
 
+function handleAnswerError(error){
+  console.log('Error on answer channel' + error)
+}
 
-// Event to trigger the room creation and setting the client as the initiator
-socket.on("init", function (room) {
-  console.log("Created room " + room);
-  isInitiator = true;
-});
+// Set Local Description and send a message to a room 
+function setLocalForwardMessage(sessionDescription){
+  peerConnection.setLocalDescription(sessionDescription)
+  sendMessage(sessionDescription, roomId)
+}
 
+// If initiator, it need to make the initial connection
+function initConnection() {
+  console.log("Init connection values: Started is ", isStarted +", and ChannelReady is:" + isChannelReady);
+  if (!isStarted && isChannelReady) {
+    console.log("Starting creation of Peer Connection");
+    createPeerConnection();
+    isStarted = true;
+    console.log("isInitiator value: ", isInitiator);
+    if (isInitiator) {
+      // if is the initiator, he must do offer for P2P connection
+      doOffer();
+    }
+  }
+}
+
+// Offer and Answer functions
+
+// do an offer with trigger error in case of failure
+function doOffer(){
+  console.log("Sending offer to peer")
+  peerConnection.createOffer(setLocalForwardMessage, handleOfferError)
+}
+
+function doAnswer(){
+  console.log("Sending answer to the peer")
+  peerConnection.createAnswer().then(
+    setLocalForwardMessage,
+    handleAnswerError,
+  );
+}
+
+function dispose(){
+  console.log("Dispose calling, client leaves")
+  leaveSetup()
+  sendMessage("close", roomId)
+}
+
+function leaveSetup(){
+  isStarted = false // connection is not started
+  peerConnection.close() // connection peer closing
+  peerConnection = null  // reset value to null for new connection setting
+}
+
+// Closing connection
+window.onbeforeunload = () => {
+  sendMessage("close", roomId);
+}
+
+// 
 
 
